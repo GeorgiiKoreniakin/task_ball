32a0b4e0d4a0f93f5a73d4907c28e13fe4abaab2
Georgii Koreniakin <applegeorgii@gmail.com>
2023-10-04 17:32:55 +0300

    fix

--- ball.py
+++ ball.py
@@ -1,26 +1,30 @@
+"""Main module."""
+
 import math
 
 
+def degree(time: float, acc: float, rad: float, vel: float = 0) -> float:
+    """Calc angle of deflection.
 
-def degree(t: float, a: float, r: float, v: float = 0) -> float:
-    # Calculating the angular displacement using the formula S = V0 * t + (a*t**2)/2
-    S = v * t + (a * t ** 2) / 2
-    
+    Args:
+        time (float): time
+        acc (float): acceleration
+        rad (float): radius
+        vel (float): velocity. Defaults to 0.
+
+    Returns:
+        float: angle of deflection
+    """
+    # Calculating the angular displacement
+    S = vel * time + (acc * time ** 2) / 2
+
     # Calculate the circumference using the formula C = 2*pi*r
-    C = 2 * math.pi * r
-    
-    # Calculate the angle in radians by dividing the displacement by the circumference
+    C = 2 * math.pi * rad
+
+    # Calculate the angle in radians
     radians = S / C
-    
+
     # Convert the angle from radians to degrees
-    print(radians)
-    print(round(radians))
     degrees = 360 * abs(math.floor(radians) - radians)
-    
+
     return round(degrees, 2)
-
-result = degree(5, 3, 10)
-print(result)
-
-
-
--- test_ball.py
+++ test_ball.py
@@ -1,4 +1,4 @@
-"""Test modul"""
+"""Test module."""
 
 import pytest
 
